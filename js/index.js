6fd3c490d0b8422b476a0599ef97724e19b34575
alkesh <alkesh9639@gmail.com>
2023-10-05 11:40:12 +0530

    add animation

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ let b = document.querySelector('.main-side')
 let c = document.querySelector('.back')
 
 
+
 a.addEventListener('click', () => {
     b.style.display = "block"
     b.classList.add('slideIn');
@@ -15,3 +16,11 @@ a.addEventListener('click', () => {
 c.addEventListener('click', () =>{
     b.style.display = "none"
 })
+
+const ani = document.querySelector('.animation')
+const ani_sec = document.querySelector('.ani-sec')
+
+setTimeout( ()=>{
+    ani.style.display = 'none'
+    ani_sec.style.display = 'block'
+},5000)
